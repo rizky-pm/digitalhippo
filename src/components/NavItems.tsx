5f2b9a361cfca198e240d6178bf426d8b1df3d5b
Rizky P. Mahendra <rizkymahendra2346@gmail.com>
2024-02-05 10:09:58 +0700

    completed project

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+
+import { PRODUCT_CATEGORIES } from '@/config';
+import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+
+  //   BOOKMARK: Clicking outside element
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener('keydown', handler);
+
+    return () => {
+      document.removeEventListener('keydown', handler);
+    };
+  }, []);
+
+  return (
+    <div className='flex gap-4 h-full' ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
